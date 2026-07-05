df1f3918fd1df5ebd71339c92bcaf3213912018c
Noob-Coder2 <krish5002ag@gmail.com>
2026-07-05 13:16:32 +0530

    chore: comment out server proxy configuration in vite.config.js for vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5001',
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:5001',
+  //   },
+  // },
 });
